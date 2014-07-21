c149937cd6ee6de8efdde12fd17cb00eda9721f8
Janet Kim <ajung119@yahoo.com>
2014-07-20 22:41:37 -0700

    test
    checking to see if I know how to push my git branch to git repository

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## testing submission
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
